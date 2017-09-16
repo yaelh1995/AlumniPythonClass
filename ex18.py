27b6991c53910fa1ffea515d0b97624eb1061de4
yaelh1995 <yaelh1995@gmail.com>
2017-09-16 13:46:13 -0400

    exercise 18

---
+++ ex18.py
@@ -0,0 +1,32 @@
+'''def print_two(*args):
+    arg1, arg2 = args
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+def print_two_again(arg1, arg2):
+    print(f"arg1: {arg1}, arg2: {arg2}"
+
+def print_one(arg1):
+    print(f"arg1: {arg1}")
+
+def print_none():
+    print("I got nothin'.")
+
+print_two("Zed","Shaw")'''
+
+'''def function(x, y, z):
+    answer = (x + y + z) / 3
+
+    print(f"""
+    The sum of all three numbers
+     divided by three is {answer}.""")
+
+function(1, 2, 3)'''
+
+
+def function(x, y, z):
+    sum = (x + y + z)
+    print(f"The sum is {sum}.")
+
+function(19, -8, 9)
+function(15, 9, 8)
+function(30, -29, 5)
